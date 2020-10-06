cbdac91846f49f4d3e70e476ea554f3fb1eeb0a9
Vawser <ben.rouncefield@gmail.com>
2020-10-06 17:35:30 +0100

    Update
    - Changed most of the Primordial enemies to now have a chance to drop Primordial Essence, rather than most of them having a 100% chance to drop.
    - Changed the Frost AI disable effect to only occur if the subject is considered in combat.

--- Cinders/event/common_func.emevd.dcx.js
+++ Cinders/event/common_func.emevd.dcx.js
@@ -6483,7 +6483,9 @@ Event(20090100, Restart, function(X0_4) {
 // <entity id>, <speffect trigger>
 //----------------------------------------------
 Event(20090200, Restart, function(X0_4, X4_4) {
-    IfCharacterHasSpeffect(MAIN, X0_4, X4_4, true, ComparisonType.Equal, 1);
+    IfCharacterHasSpeffect(AND_01, X0_4, X4_4, true, ComparisonType.Equal, 1);
+    IfCharacterAIState(AND_01, X0_4, AIStateType.Combat, ComparisonType.Equal, 1);
+    IfConditionGroup(MAIN, PASS, AND_01);
     
     SetSpeffect(X0_4, 180021010); // Disable Frost for 5 seconds
     SetCharacterAIState(X0_4, Disabled);
