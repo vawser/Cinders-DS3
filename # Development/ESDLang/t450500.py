53421f219741ce7ce9341f6508741062bc7351fd
Vawser <ben.rouncefield@gmail.com>
2021-02-12 21:13:49 +0000

    Update
    - Added t450500 esd

--- # Development/ESDLang/t450500.py
+++ # Development/ESDLang/t450500.py
@@ -1,38 +1,9 @@
-#-------------------------------------------
-#-- Boss Revival: Sister Friede
-#-------------------------------------------
 # -*- coding: utf-8 -*-
-
-#----------------------------------------------------
-# Main Loop
-#----------------------------------------------------
 def t450500_1():
     """ State 0,1 """
-    assert GetCurrentStateElapsedTime() > 1
-    """ State 2 """
-    while True:
-        call = t450500_x0() # Host Player
-        assert IsClientPlayer() == 1
-        """ State 3 """
-        call = t450500_x1() # Client Player
-        assert not IsClientPlayer()
+    t450500_x4()
 
-# Host Player
 def t450500_x0():
-    """ State 0,1 """
-    while True:
-        call = t450500_x3()
-        assert not GetEventStatus(1000) and not GetEventStatus(1001) and not GetEventStatus(1002)
-
-# Client Player
-def t450500_x1():
-    """ State 0,1 """
-    assert t450500_x2() # Clear Talk State
-    """ State 2 """
-    return 0
-
-# Clear Talk State
-def t450500_x2():
     """ State 0,1 """
     if not CheckSpecificPersonTalkHasEnded(0):
         """ State 7 """
@@ -56,186 +27,291 @@ def t450500_x2():
         pass
     """ State 8 """
     return 0
-    
-# Check Death
-def t450500_x3():
+
+def t450500_x1(z2=17, z3=9019, flag3=6067):
     """ State 0,1 """
-    call = t450500_x4() # NPC Loop
-    assert CheckSelfDeath() == 1
+    if GetEventStatus(flag3) == 1:
+        """ State 2 """
+        pass
+    else:
+        """ State 3,4 """
+        AcquireGesture(z2)
+        OpenItemAcquisitionMenu(3, z3, 1)
+        SetEventState(flag3, 1)
+        assert not IsMenuOpen(63) and GetCurrentStateElapsedFrames() > 1
+    """ State 5 """
     return 0
 
-# NPC Loop
-def t450500_x4():
-    """ State 0,5 """
-    while True:
-        call = t450500_x5(z4=6120, flag4=1015, flag5=6000, flag6=6000, flag7=6000, flag8=6000) # Interaction State
-        if call.Done():
-            """ State 3 """
-            call = t450500_x8() # Menu Pre-loop
-            if call.Done():
-                pass
-            elif IsAttackedBySomeone() == 1:
-                """ State 1 """
-                Label('L0')
-                call = t450500_x6() # Attack Check
-                def ExitPause():
-                    RemoveMyAggro()
-                if call.Done():
-                    pass
-                elif IsPlayerDead() == 1:
-                    break
-            elif IsPlayerDead() == 1:
-                break
-            elif GetDistanceToPlayer() > 3 or GetPlayerYDistance() > 0.25:
-                """ State 4 """
-                call = t450500_x7() # Distance Check
-                if call.Done() and (GetDistanceToPlayer() < 2.5 and GetPlayerYDistance() < 0.249):
-                    pass
-                elif IsAttackedBySomeone() == 1:
-                    Goto('L0')
-        elif IsAttackedBySomeone() == 1:
-            Goto('L0')
-        elif IsPlayerDead() == 1:
-            break
-    """ State 2 """
-    t450500_x2() # Clear Talk State
-    
-# Interaction State
-def t450500_x5(z4=6120, flag4=1015, flag5=6000, flag6=6000, flag7=6000, flag8=6000):
+def t450500_x2(z1=_, flag1=6001, flag2=6000):
     """ State 0,1 """
     while True:
         assert (not GetOneLineHelpStatus() and not IsTalkingToSomeoneElse() and not IsClientPlayer()
                 and not IsPlayerDead() and not IsCharacterDisabled())
-        """ State 3 """
-        assert (GetEventStatus(flag4) == 1 or GetEventStatus(flag5) == 1 or GetEventStatus(flag6) ==
-                1 or GetEventStatus(flag7) == 1 or GetEventStatus(flag8) == 1)
         """ State 2 """
-        if (not (not GetOneLineHelpStatus() and not IsTalkingToSomeoneElse() and not IsClientPlayer()
-            and not IsPlayerDead() and not IsCharacterDisabled())):
+        assert CompareBonfireState(1)
+        """ State 4 """
+        assert GetEventStatus(flag1) == 1 and not GetEventStatus(flag2)
+        """ State 3 """
+        if CompareBonfireState(0):
             pass
-        elif (not GetEventStatus(flag4) and not GetEventStatus(flag5) and not GetEventStatus(flag6) and
-              not GetEventStatus(flag7) and not GetEventStatus(flag8)):
-            pass
-        elif CheckActionButtonArea(z4):
+        elif CheckActionButtonArea(z1):
             break
-    """ State 4 """
+        elif (not (not GetOneLineHelpStatus() and not IsTalkingToSomeoneElse() and not IsClientPlayer()
+              and not IsPlayerDead() and not IsCharacterDisabled())):
+            pass
+        elif not GetEventStatus(flag1) or GetEventStatus(flag2) == 1:
+            pass
+    """ State 5 """
     return 0
 
-# Attack Check
+def t450500_x3(action1=10010713):
+    """ State 0,1 """
+    # action:10010713:Game installation incomplete.\nCannot travel between bonfires.
+    OpenGenericDialog(1, action1, 1, 0, 1)
+    assert not CheckSpecificPersonGenericDialogIsOpen(0)
+    """ State 2 """
+    return 0
+
+def t450500_x4():
+    """ State 0,1 """
+    while True:
+        call = t450500_x5()
+        assert IsMultiplayerInProgress() == 1
+        """ State 2 """
+        call = t450500_x8()
+        assert not IsMultiplayerInProgress()
+
+def t450500_x5():
+    """ State 0,1 """
+    if CompareBonfireLevel(5, 0) == 1:
+        """ State 2 """
+        Label('L0')
+    else:
+        """ State 3,15 """
+        call = t450500_x2(z1=6100, flag1=6001, flag2=6000)
+        if call.Done():
+            """ State 7 """
+            TurnCharacterToFaceEntity(-1, 10000, -1)
+            assert GetCurrentStateElapsedFrames() > 1 and GetWhetherChrEventAnimHasEnded(10000) == 1
+            """ State 4 """
+            OfferHumanity()
+            assert CompareBonfireLevel(5, 0) == 1
+            """ State 11 """
+            RequestUnlockTrophy(41)
+            """ State 9 """
+            UpdatePlayerRespawnPoint()
+            Goto('L0')
+        elif CompareBonfireLevel(5, 0) == 1:
+            pass
+    """ State 14 """
+    assert t450500_x2(z1=6101, flag1=6001, flag2=6000)
+    """ State 5 """
+    ClearPlayerDamageInfo()
+    """ State 6 """
+    SetTalkTime(1)
+    """ State 8 """
+    TurnCharacterToFaceEntity(-1, 10000, -1)
+    assert GetCurrentStateElapsedFrames() > 1 and GetWhetherChrEventAnimHasEnded(10000) == 1
+    """ State 10 """
+    UpdatePlayerRespawnPoint()
+    """ State 12 """
+    StartBonfireAnimLoop()
+    call = t450500_x9()
+    def ExitPause():
+        EndBonfireKindleAnimLoop()
+    if call.Done():
+        Goto('L0')
+    elif HasPlayerBeenAttacked() == 1 or GetDistanceToPlayer() > 3 or CompareBonfireState(0):
+        """ State 13 """
+        assert t450500_x10()
+        Goto('L0')
+
 def t450500_x6():
     """ State 0,6 """
-    assert t450500_x2() # Clear Talk State
-    """ State 3 """
-    assert GetCurrentStateElapsedFrames() > 1
-    """ State 1 """
-    assert not GetEventStatus(1016) and not GetEventStatus(1017)
-    """ State 2 """
-    if GetDistanceToPlayer() > 12:
-        """ State 7 """
-        assert t450500_x2() # Clear Talk State
-    else:
-        """ State 5 """
+    call = t450500_x0()
+    if call.Done() and CompareBonfireLevel(5, 0) == 1:
         pass
-    """ State 9 """
-    return 0
+    elif call.Done():
+        """ State 2,7 """
+        call = t450500_x2(z1=6100, flag1=6001, flag2=6000)
+        if call.Done():
+            """ State 4 """
+            TurnCharacterToFaceEntity(-1, 10000, -1)
+            assert GetCurrentStateElapsedFrames() > 1 and GetWhetherChrEventAnimHasEnded(10000) == 1
+            """ State 3 """
+            OfferHumanity()
+            """ State 5 """
+            UpdatePlayerRespawnPoint()
+            assert CompareBonfireLevel(5, 0) == 1
+        elif CompareBonfireLevel(5, 0) == 1:
+            pass
+    """ State 1 """
 
-# Distance Check
 def t450500_x7():
     """ State 0,1 """
-    if (CheckSpecificPersonMenuIsOpen(-1, 0) == 1 and not CheckSpecificPersonMenuIsOpen(12, 0) and not
-        CheckSpecificPersonGenericDialogIsOpen(0)):
-        """ State 2,5 """
-        if GetDistanceToPlayer() > 12:
-            """ State 4 """
-            Label('L0')
-            assert t450500_x2() # Clear Talk State
-    else:
-        """ State 3 """
-        Goto('L0')
-    """ State 6 """
+    assert t450500_x0()
+    """ State 2 """
     return 0
-    
-# Menu Pre-loop
+
 def t450500_x8():
     """ State 0,1 """
-    assert t450500_x9()
-    """ State 24 """
-    return 0
-    
+    while True:
+        call = t450500_x6()
+        assert IsClientPlayer() == 1
+        """ State 2 """
+        call = t450500_x7()
+        assert not IsClientPlayer()
+
 def t450500_x9():
+    """ State 0,10 """
+    assert GetCurrentStateElapsedTime() > 2
+    """ State 21 """
+    assert t450500_x1(z2=17, z3=9019, flag3=6067)
+    """ State 17 """
     c1110()
-    
+    """ State 1 """
     while True:
         ClearTalkListData()
+        """ State 2 """
+        # Travel
+        AddTalkListData(1, 15000150, -1)
         
-        # Revive
-        AddTalkListDataIf(GetEventStatus(25001020) == 1 and GetEventStatus(14500800) == 1, 1, 99002850, -1) 
+        # Level Up
+        AddTalkListData(10, 15002000, -1)
         
-        # Leave
+        #Allot Estus
+        # AddTalkListData(22, 15002002, -1)
+        
+        # Attune Spell
+        AddTalkListData(2, 15000130, -1)
+        
+        # Reinforce Weapon
+        AddTalkListData(18, 15010002, -1)
+        
+        # Infuse Weapon
+        AddTalkListData(17, 15010001, -1)
+        
+        # Repair Equipment
+        AddTalkListData(19, 15010003, -1)
+        
+        # Purchase Item
+        AddTalkListData(20, 15010005, -1)
+        
+        # Sell Item
+        AddTalkListData(21, 15010006, -1)
+        
+        # Organize Storage Box
+        AddTalkListData(3, 15000220, -1)
+        
+        # action:15000005:Leave
         AddTalkListData(99, 15000005, -1)
-        
-        assert (not CheckSpecificPersonGenericDialogIsOpen(2) and not (CheckSpecificPersonMenuIsOpen(-1,
-                2) == 1 and not CheckSpecificPersonGenericDialogIsOpen(2)))
+        """ State 4 """
         ShowShopMessage(1)
-        
-        # Revive 
         if GetTalkListEntryResult() == 1:
-            assert t450500_x52(99002620, 400, 1)
+            """ State 3 """
+            if GetEventStatus(2030) == 1:
+                """ State 18,8 """
+                StartWarpMenuInit(-1)
+                assert GetCurrentStateElapsedFrames() > 1
+                """ State 12 """
+                if WasWarpMenuDestinationSelected() == 1:
+                    break
+                elif not (CheckSpecificPersonMenuIsOpen(-1, 0) == 1 and not CheckSpecificPersonGenericDialogIsOpen(0)):
+                    """ State 13 """
+                    pass
+            else:
+                """ State 16,20 """
+                # action:10010713:Game installation incomplete.\nCannot travel between bonfires.
+                assert t450500_x3(action1=10010713)
+        elif GetTalkListEntryResult() == 2:
+            """ State 6,7 """
+            OpenMagicEquip(1000, 1000)
+            assert not (CheckSpecificPersonMenuIsOpen(11, 0) == 1 and not CheckSpecificPersonGenericDialogIsOpen(0))
+        elif GetTalkListEntryResult() == 3:
+            """ State 14,15 """
+            OpenRepository()
+            assert not (CheckSpecificPersonMenuIsOpen(3, 0) == 1 and not CheckSpecificPersonGenericDialogIsOpen(0))
+        elif GetTalkListEntryResult() == 10:
+            """ State 70,71 """
+            OpenSoul()
+            assert (not CheckSpecificPersonGenericDialogIsOpen(2) and not (CheckSpecificPersonMenuIsOpen(-1,
+                    2) == 1 and not CheckSpecificPersonGenericDialogIsOpen(2)))
+            continue
+        elif GetTalkListEntryResult() == 22:
+            """State 97,98"""
+            OpenEstusAllotMenu()
+            assert not (CheckSpecificPersonMenuIsOpen(14, 0) == 1 and not CheckSpecificPersonGenericDialogIsOpen(0))
+            continue
+        elif GetTalkListEntryResult() == 17:
+            """ State 84,85 """
+            CombineMenuFlagAndEventFlag(6001, 344)
+            CombineMenuFlagAndEventFlag(6001, 337)
+            CombineMenuFlagAndEventFlag(6001, 334)
+            CombineMenuFlagAndEventFlag(300, 332)
+            CombineMenuFlagAndEventFlag(300, 333)
+            CombineMenuFlagAndEventFlag(300, 342)
+            CombineMenuFlagAndEventFlag(301, 335)
+            CombineMenuFlagAndEventFlag(301, 345)
+            CombineMenuFlagAndEventFlag(301, 340)
+            CombineMenuFlagAndEventFlag(302, 336)
+            CombineMenuFlagAndEventFlag(302, 338)
+            CombineMenuFlagAndEventFlag(302, 339)
+            CombineMenuFlagAndEventFlag(303, 341)
+            CombineMenuFlagAndEventFlag(303, 343)
+            CombineMenuFlagAndEventFlag(303, 346)
+            CombineMenuFlagAndEventFlag(6000, 347)
+            CombineMenuFlagAndEventFlag(6001, 331)
+            CombineMenuFlagAndEventFlag(6001, 232)
+            CombineMenuFlagAndEventFlag(6001, 233)
+            CombineMenuFlagAndEventFlag(6001, 234)
+            CombineMenuFlagAndEventFlag(6001, 235)
+                
+            """ State 86 """
+            OpenEquipmentChangeOfPurposeShop()
+            assert (not CheckSpecificPersonGenericDialogIsOpen(2) and not (CheckSpecificPersonMenuIsOpen(-1,
+                    2) == 1 and not CheckSpecificPersonGenericDialogIsOpen(2)))
+            continue
+        elif GetTalkListEntryResult() == 18:
+            """ State 87,88 """
+            CombineMenuFlagAndEventFlag(6001, 232)
+            CombineMenuFlagAndEventFlag(6001, 233)
+            CombineMenuFlagAndEventFlag(6001, 234)
+            CombineMenuFlagAndEventFlag(6001, 235)
+            
+            """ State 89 """
+            OpenEnhanceShop(0)
+            assert (not CheckSpecificPersonGenericDialogIsOpen(2) and not (CheckSpecificPersonMenuIsOpen(-1,
+                    2) == 1 and not CheckSpecificPersonGenericDialogIsOpen(2)))
+            continue
+        elif GetTalkListEntryResult() == 19:
+            """ State 90 """
+            OpenRepairShop()
+            assert (not CheckSpecificPersonGenericDialogIsOpen(2) and not (CheckSpecificPersonMenuIsOpen(-1,
+                    2) == 1 and not CheckSpecificPersonGenericDialogIsOpen(2)))
+            continue
+        elif GetTalkListEntryResult() == 20:
+            """ State 91,92 """
+            OpenRegularShop(36000, 36999)
+            assert (not CheckSpecificPersonGenericDialogIsOpen(2) and not (CheckSpecificPersonMenuIsOpen(-1,
+                    2) == 1 and not CheckSpecificPersonGenericDialogIsOpen(2)))
+            continue
+        elif GetTalkListEntryResult() == 21:
+            """ State 93,94 """
+            OpenSellShop(-1, -1)
+            assert (not CheckSpecificPersonGenericDialogIsOpen(2) and not (CheckSpecificPersonMenuIsOpen(-1,
+                    2) == 1 and not CheckSpecificPersonGenericDialogIsOpen(2)))
+            continue
+        elif (GetTalkListEntryResult() == 99 or not (CheckSpecificPersonMenuIsOpen(1, 0) == 1 and not
+              CheckSpecificPersonGenericDialogIsOpen(0))):
+            """ State 5,22 """
             return 0
-        elif not (CheckSpecificPersonMenuIsOpen(1, 0) == 1 and not CheckSpecificPersonGenericDialogIsOpen(0)):
-            return 0
+    """ State 11,19 """
+    SetEventState(74000013, 1)
+    """ State 9 """
 
-#----------------------------------------------------
-# Utility
-#----------------------------------------------------
-def t450500_x50(action1=_):
+def t450500_x10():
     """ State 0,1 """
-    OpenGenericDialog(8, action1, 3, 4, 2)
-    assert not CheckSpecificPersonGenericDialogIsOpen(0)
+    assert t450500_x0()
     """ State 2 """
-    if GetGenericDialogButtonResult() == 1:
-        """ State 3 """
-        return 0
-    else:
-        """ State 4 """
-        return 1
-
-def t450500_x51(action1=_):
-    """ State 0,1 """
-    OpenGenericDialog(8, action1, 0, 3, 2)
-    assert not CheckSpecificPersonGenericDialogIsOpen(0)
-    """ State 2 """
-    if GetGenericDialogButtonResult() == 1:
-        """ State 3 """
-        return 0
-    else:
-        """ State 4 """
-        return 1
-        
-# Revive
-def t450500_x52(text1=_, base_cost=_, derived_stat=_):
-    """ State 0,1 """
-    SetAquittalCostMessageTag(base_cost, derived_stat)
-    """ State 14 """
-    call = t450500_x50(action1=text1)
-    
-    if call.Get() == 0:
-        """ State 7 """
-        if ComparePlayerAcquittalPrice(base_cost, derived_stat, 2) == 1:
-            """ State 4,13 """
-            assert t450500_x50(action1=13000050) # Lack souls
-        else:
-            """ State 5 """
-            SubtractAcquittalCostFromPlayerSouls(base_cost, derived_stat)
-            """ State 6 """
-            SetEventState(14500800, 0);
-            SetEventState(9322, 0);
-            SetEventState(6322, 0);
-            SetEventState(14500000, 0);
-            SetEventState(14500162, 0); # Dreg Heap warp
-            GiveSpEffectToPlayer(260100200)
-    elif call.Get() == 1:
-        """ State 8 """
-        pass
-
     return 0
+
