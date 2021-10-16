3aab082f6f4a2085e4d38853cea2fe27e095860a
Vawser <ben.rouncefield@gmail.com>
2021-10-16 13:28:00 +0100

    AI
    - Forgotten Locklin: now has a customised AI.

--- # Development/LUA AI/m33_00_00_00-luabnd-dcx/script/ai/out/bin/030100_battle.lua
+++ # Development/LUA AI/m33_00_00_00-luabnd-dcx/script/ai/out/bin/030100_battle.lua
@@ -39,13 +39,13 @@ Goal.Activate = function (self, ai, goal)
     -- Act Distribution
     ----------------------------------
     if distance >= 7 then
-        actChanceList[1] = 0 -- Right Light Attack + Approach
-        actChanceList[2] = 0 -- Right Heavy Attack + Approach
+        actChanceList[1] = 10 -- Right Light Attack + Approach
+        actChanceList[2] = 10 -- Right Heavy Attack + Approach
         actChanceList[3] = 0 -- Kick + Approach
         actChanceList[4] = 0 -- Jump Attack + Approach
         actChanceList[5] = 0 -- WA: Kindled Flurry
         
-        actChanceList[10] = 0 -- Approach + Running Attack
+        actChanceList[10] = 20 -- Approach + Running Attack
         actChanceList[11] = 0 -- Backstep Roll
         actChanceList[12] = 0 -- Forward Roll + Run + Basic Light Attack
         actChanceList[13] = 0 -- Side Roll + Run + Basic Light Attack
@@ -54,48 +54,48 @@ Goal.Activate = function (self, ai, goal)
         actChanceList[16] = 0 -- Backstep Walk
         actChanceList[17] = 0 -- Approach
         
-        actChanceList[30] = 0 -- Cast Spell (Slot 0) - Heave Earth
+        actChanceList[30] = 20 -- Cast Spell (Slot 0) - Heave Earth
         actChanceList[31] = 0 -- Cast Spell (Slot 1) - Earthquake
-        actChanceList[32] = 0 -- Cast Spell (Slot 2) - Earthfall
+        actChanceList[32] = 20 -- Cast Spell (Slot 2) - Earthfall
         actChanceList[33] = 0 -- Cast Spell (Slot 3) - Rockflesh
     elseif distance >= 3 then
-        actChanceList[1] = 0 -- Right Light Attack + Approach
-        actChanceList[2] = 0 -- Right Heavy Attack + Approach
+        actChanceList[1] = 10 -- Right Light Attack + Approach
+        actChanceList[2] = 10 -- Right Heavy Attack + Approach
         actChanceList[3] = 0 -- Kick + Approach
-        actChanceList[4] = 0 -- Jump Attack + Approach
-        actChanceList[5] = 0 -- WA: Kindled Flurry
+        actChanceList[4] = 10 -- Jump Attack + Approach
+        actChanceList[5] = 10 -- WA: Kindled Flurry
         
-        actChanceList[10] = 0 -- Approach + Running Attack
+        actChanceList[10] = 10 -- Approach + Running Attack
         actChanceList[11] = 0 -- Backstep Roll
         actChanceList[12] = 0 -- Forward Roll + Run + Basic Light Attack
         actChanceList[13] = 0 -- Side Roll + Run + Basic Light Attack
         actChanceList[14] = 0 -- Back Roll + Basic Light Attack
-        actChanceList[15] = 0 -- Strafe
+        actChanceList[15] = 10 -- Strafe
         actChanceList[16] = 0 -- Backstep Walk
         actChanceList[17] = 0 -- Approach
         
-        actChanceList[30] = 0 -- Cast Spell (Slot 0) - Heave Earth
-        actChanceList[31] = 0 -- Cast Spell (Slot 1) - Earthquake
+        actChanceList[30] = 20 -- Cast Spell (Slot 0) - Heave Earth
+        actChanceList[31] = 20 -- Cast Spell (Slot 1) - Earthquake
         actChanceList[32] = 0 -- Cast Spell (Slot 2) - Earthfall
         actChanceList[33] = 0 -- Cast Spell (Slot 3) - Rockflesh
     else
-        actChanceList[1] = 0 -- Right Light Attack + Approach
-        actChanceList[2] = 0 -- Right Heavy Attack + Approach
-        actChanceList[3] = 0 -- Kick + Approach
-        actChanceList[4] = 0 -- Jump Attack + Approach
+        actChanceList[1] = 20 -- Right Light Attack + Approach
+        actChanceList[2] = 20 -- Right Heavy Attack + Approach
+        actChanceList[3] = 20 -- Kick + Approach
+        actChanceList[4] = 5 -- Jump Attack + Approach
         actChanceList[5] = 0 -- WA: Kindled Flurry
         
         actChanceList[10] = 0 -- Approach + Running Attack
-        actChanceList[11] = 0 -- Backstep Roll
-        actChanceList[12] = 0 -- Forward Roll + Run + Basic Light Attack
-        actChanceList[13] = 0 -- Side Roll + Run + Basic Light Attack
-        actChanceList[14] = 0 -- Back Roll + Basic Light Attack
-        actChanceList[15] = 0 -- Strafe
+        actChanceList[11] = 10 -- Backstep Roll
+        actChanceList[12] = 10 -- Forward Roll + Run + Basic Light Attack
+        actChanceList[13] = 10 -- Side Roll + Run + Basic Light Attack
+        actChanceList[14] = 10 -- Back Roll + Basic Light Attack
+        actChanceList[15] = 10 -- Strafe
         actChanceList[16] = 0 -- Backstep Walk
         actChanceList[17] = 0 -- Approach
         
         actChanceList[30] = 0 -- Cast Spell (Slot 0) - Heave Earth
-        actChanceList[31] = 0 -- Cast Spell (Slot 1) - Earthquake
+        actChanceList[31] = 10 -- Cast Spell (Slot 1) - Earthquake
         actChanceList[32] = 0 -- Cast Spell (Slot 2) - Earthfall
         actChanceList[33] = 0 -- Cast Spell (Slot 3) - Rockflesh
     end
@@ -103,35 +103,38 @@ Goal.Activate = function (self, ai, goal)
     ----------------------------------
     -- Act Modifiers
     ----------------------------------
-    -- if ai:IsTargetGuard(TARGET_ENE_0) then
-        -- actChanceList[31] = actChanceList[31] + 20 -- Cast Spell (Slot 1) - Earthquake
-    -- end
+    if ai:IsTargetGuard(TARGET_ENE_0) then
+        actChanceList[5] = actChanceList[5] + 30 -- WA: Kindled Flurry
+    end
     
-     -- -- Block repeat usage of Wrathful Orbs
-    -- ai:AddObserveSpecialEffectAttribute(TARGET_SELF, 160770010)
+    -- Use Rockflesh once below 50% HP
+    if hp_rate <= 0.5 then
+        actChanceList[33] = 100 -- Cast Spell (Slot 3) - Rockflesh
+    end
     
-    -- if ai:HasSpecialEffectId(TARGET_SELF, 160770010) then
-        -- actChanceList[34] = 0 -- Cast Spell (Slot 4) - Wrathful Orbs
-    -- end
+     -- Block repeat usage of Rockflesh
+    ai:AddObserveSpecialEffectAttribute(TARGET_SELF, 160500200)
     
-    -- -- Disable spells when silenced so the Spurned Shade doesn't waste time trying to use them
-    -- ai:AddObserveSpecialEffectAttribute(TARGET_SELF, 103581000)
-    -- ai:AddObserveSpecialEffectAttribute(TARGET_SELF, 103581010)
+    if ai:HasSpecialEffectId(TARGET_SELF, 160500200) then
+        actChanceList[33] = 0 -- Cast Spell (Slot 3) - Rockflesh
+    end
     
-    -- if ai:HasSpecialEffectId(TARGET_SELF, 103581000) or ai:HasSpecialEffectId(TARGET_SELF, 103581010) then
-        -- actChanceList[1] = 15 -- Right Light Attack + Approach
-        -- actChanceList[2] = 15 -- Right Heavy Attack + Approach
-        -- actChanceList[3] = 10 -- Kick + Approach
-        -- actChanceList[4] = 10 -- Jump Attack + Approach
-        
-        -- actChanceList[30] = 0 -- Cast Spell (Slot 0) - Heave Earth
-        -- actChanceList[31] = 0 -- Cast Spell (Slot 1) - Earthquake
-        -- actChanceList[32] = 0 -- Cast Spell (Slot 2) - Earthfall
-        -- actChanceList[33] = 0 -- Cast Spell (Slot 3) - Rockflesh
-    -- end
+    -- Block WA if stamina when low on stamina
+    if stamina < 80 then
+        actChanceList[5] = 0 -- WA: Kindled Flurry
+    end
+    
+    -- Block dash and rolls when low on stamina
+    if stamina < 20 then
+        actChanceList[10] = 0 -- Approach + Running Attack
+        actChanceList[11] = 0 -- Backstep Roll
+        actChanceList[12] = 0 -- Forward Roll + Run + Basic Light Attack
+        actChanceList[13] = 0 -- Side Roll + Run + Basic Light Attack
+        actChanceList[14] = 0 -- Back Roll + Basic Light Attack
+    end
     
     ----------------------------------
-    -- Act Checks
+    -- Movement Checks
     ----------------------------------
     -- Block backstep if there is an obstacle behind the AI within 2.6 meters
     if SpaceCheck(ai, goal, 180, ai:GetStringIndexedNumber("Dist_BackStep")) == false then
@@ -166,20 +169,6 @@ Goal.Activate = function (self, ai, goal)
     -- Block strafe if there is an obstacle +/- 90 degrees to the side of the AI within 1 meters
     if SpaceCheck(ai, goal, -90, 1) == false and SpaceCheck(ai, goal, 90, 1) == false then
         actChanceList[15] = 0 -- Strafe
-    end
-    
-    -- Block dash and rolls when low on stamina
-    if stamina < 20 then
-        actChanceList[10] = 0 -- Approach + Running Attack
-        actChanceList[11] = 0 -- Backstep Roll
-        actChanceList[12] = 0 -- Forward Roll + Run + Basic Light Attack
-        actChanceList[13] = 0 -- Side Roll + Run + Basic Light Attack
-        actChanceList[14] = 0 -- Back Roll + Basic Light Attack
-    end
-    
-    -- Block WA if stamina when low on stamina
-    if stamina < 80 then
-        actChanceList[5] = 0 -- WA: Kindled Flurry
     end
     
     ----------------------------------
@@ -828,6 +817,7 @@ Goal.Interrupt = function (self, ai, goal)
     local distance  = ai:GetDist(TARGET_ENE_0)
     local roll      = ai:GetRandam_Int(1, 100)
     
+    -- Occurs if the player has been guard broken
     if ai:IsInterupt(INTERUPT_GuardBreak) and distance < 3 then
         goal:ClearSubGoal()
         
@@ -837,11 +827,21 @@ Goal.Interrupt = function (self, ai, goal)
         goal:AddSubGoal(GOAL_COMMON_AttackTunableSpin, 10, NPC_ATK_R1, TARGET_ENE_0, 999, 0, -1) -- Right Light Attack + Approach
         
         return true
+    -- Occurs if the player is vulnerable to a parry
+    elseif ai:IsInterupt(INTERUPT_ParryTiming) then
+        if not ai:IsBothHandMode(TARGET_SELF) then
+            if distance < 2 and roll <= 50 and 20 <= stamina then
+                goal:ClearSubGoal()
+                goal:AddSubGoal(GOAL_COMMON_AttackTunableSpin, 0.05, NPC_ATK_L2, TARGET_ENE_0, 999, 0, 0) -- Left WA (Shield Bash)
+                return true
+            end
+        end
+    -- Occurs when the AI looks for an attack
     elseif ai:IsInterupt(INTERUPT_FindAttack) then
         if distance < 1.8 and roll <= 80 then
             if roll <= 60 and 30 <= stamina then
                 goal:ClearSubGoal()
-                NPC_Locklin_Act16(ai, goal, paramTbl) -- Backstep Walk
+                NPC_Locklin_Act15(ai, goal, paramTbl) -- Strafe
                 return true
             elseif stamina <= 35 and 0 <= stamina then
                 goal:ClearSubGoal()
@@ -854,7 +854,7 @@ Goal.Interrupt = function (self, ai, goal)
             return true
         end
     end
-    if ai:IsInterupt(INTERUPT_Shoot) and roll <= 33 and 20 <= stamina then
+    elseif ai:IsInterupt(INTERUPT_Shoot) and roll <= 33 and 20 <= stamina then
         goal:ClearSubGoal()
         NPC_Locklin_Act13(ai, goal) -- Side Roll + Run + Basic Light Attack
         return true
