26ff0f3c76cd4784ba0e31900cc3cae2b932b01f
Vawser <ben.rouncefield@gmail.com>
2021-11-08 00:15:23 +0000

    Map
    - Smouldering Lake:
     - Overhauled the lake area with new objects.
     - Moved Helia of the Lake to a new location.
     - Removed 2 of the sandworms.
     - Added the 2 removed sandworm treasure to 2 new corpse treasures.
     - Added 3 patrolling Demons
     - Added 1 patrolling Demon Cleric
     - Fixed several Demon Clerics in m38 that lacked fire orb support

--- # Development/ESDLang/t380501.py
+++ # Development/ESDLang/t380501.py
@@ -179,7 +179,7 @@ def t380501_x9():
             return 0
         # Talk
         elif GetTalkListEntryResult() == 3:
-            assert t380501_x10(text1=10024000, flag1=0, mode1=0)
+            assert t380501_x10(text1=10025000, flag1=0, mode1=0)
             continue
         elif not (CheckSpecificPersonMenuIsOpen(-1, 0) == 1 and not CheckSpecificPersonGenericDialogIsOpen(0)):
             return 0
