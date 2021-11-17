5ef4e3b5e59925557fe5b87552bf741ecd1d4b05
Vawser <ben.rouncefield@gmail.com>
2021-11-17 23:56:29 +0000

    Spells + Spell Tools
    - Increased max spellbuff to 300 again.
    - Removed the FP pause from casting a normal spell. Other pauses still remain.
    - Added Unstable infusion: increases spellbuff by 50, but each cast applies a +10% FP cost increase for 10 seconds, stacking.

--- # Development/LUA AI/aicommon-luabnd-dcx/script/ai/out/bin/801200_battle.lua
+++ # Development/LUA AI/aicommon-luabnd-dcx/script/ai/out/bin/801200_battle.lua
@@ -1,115 +1,144 @@
 RegisterTableGoal(GOAL_SpellSummon_UndeadDog_801200_Battle, "GOAL_SpellSummon_UndeadDog_801200_Battle")
 REGISTER_GOAL_NO_SUB_GOAL(GOAL_SpellSummon_UndeadDog_801200_Battle, true)
+
+-------------------------
+-- Initialize
+-------------------------
 Goal.Initialize = function (self, ai, goal, arg3)
     return 
 end
 
+-------------------------
+-- Activate
+-------------------------
 Goal.Activate = function (self, ai, goal)
     Init_Pseudo_Global(ai, goal)
+    
     ai:SetStringIndexedNumber("Dist_SideStep", 5)
     ai:SetStringIndexedNumber("Dist_BackStep", 5)
+    
     ai:AddObserveSpecialEffectAttribute(TARGET_SELF, 12012)
 
-    local local0 = {}
-    local local1 = {}
-    local local2 = {}
-    Common_Clear_Param(local0, local1, local2)
+    local actChanceList = {}
+    local actFuncList = {}
+    local actTblList = {}
+    
+    Common_Clear_Param(actChanceList, actFuncList, actTblList)
     
     goal:AddSubGoal(GOAL_NPC_WhiteGhost_Battle, 2)
     
-    local local3 = ai:GetDist(TARGET_ENE_0)
-    local local4 = ai:GetRandam_Int(1, 100)
-    local local5 = ai:GetExcelParam(AI_EXCEL_THINK_PARAM_TYPE__thinkAttr_doAdmirer)
-    local local6 = ai:GetEventRequest()
+    local distance = ai:GetDist(TARGET_ENE_0)
+    
     if ai:IsInsideTarget(TARGET_ENE_0, AI_DIR_TYPE_B, 180) then
-        if 6 <= local3 then
-            local0[11] = 0
-            local0[20] = 100
-        elseif 2 <= local3 then
-            local0[11] = 30
-            local0[20] = 70
+        if 6 <= distance then
+            actChanceList[11] = 0
+            actChanceList[20] = 100
+        elseif 2 <= distance then
+            actChanceList[11] = 30
+            actChanceList[20] = 70
         else
-            local0[11] = 50
-            local0[20] = 50
+            actChanceList[11] = 50
+            actChanceList[20] = 50
         end
-    elseif 7 <= local3 then
-        local0[1] = 0
-        local0[2] = 5
-        local0[3] = 40
-        local0[4] = 5
-        local0[5] = 20
-        local0[6] = 20
-        local0[7] = 0
-        local0[10] = 10
-        local0[11] = 0
-    elseif 4 <= local3 then
-        local0[1] = 10
-        local0[2] = 20
-        local0[3] = 30
-        local0[4] = 30
-        local0[5] = 0
-        local0[6] = 0
-        local0[7] = 0
-        local0[10] = 10
-        local0[11] = 0
-    elseif 2 <= local3 then
-        local0[1] = 20
-        local0[2] = 40
-        local0[3] = 0
-        local0[4] = 0
-        local0[5] = 0
-        local0[6] = 0
-        local0[7] = 20
-        local0[10] = 10
-        local0[11] = 10
+    elseif 7 <= distance then
+        actChanceList[1] = 0
+        actChanceList[2] = 5
+        actChanceList[3] = 40
+        actChanceList[4] = 5
+        actChanceList[5] = 20
+        actChanceList[6] = 20
+        actChanceList[7] = 0
+        actChanceList[10] = 10
+        actChanceList[11] = 0
+    elseif 4 <= distance then
+        actChanceList[1] = 10
+        actChanceList[2] = 20
+        actChanceList[3] = 30
+        actChanceList[4] = 30
+        actChanceList[5] = 0
+        actChanceList[6] = 0
+        actChanceList[7] = 0
+        actChanceList[10] = 10
+        actChanceList[11] = 0
+    elseif 2 <= distance then
+        actChanceList[1] = 20
+        actChanceList[2] = 40
+        actChanceList[3] = 0
+        actChanceList[4] = 0
+        actChanceList[5] = 0
+        actChanceList[6] = 0
+        actChanceList[7] = 20
+        actChanceList[10] = 10
+        actChanceList[11] = 10
     else
-        local0[1] = 30
-        local0[2] = 20
-        local0[3] = 0
-        local0[4] = 0
-        local0[5] = 0
-        local0[6] = 0
-        local0[7] = 20
-        local0[10] = 0
-        local0[11] = 30
+        actChanceList[1] = 30
+        actChanceList[2] = 20
+        actChanceList[3] = 0
+        actChanceList[4] = 0
+        actChanceList[5] = 0
+        actChanceList[6] = 0
+        actChanceList[7] = 20
+        actChanceList[10] = 0
+        actChanceList[11] = 30
     end
-    local0[3] = SetCoolTime(ai, goal, 3005, 10, local0[3], 0)
-    local0[7] = SetCoolTime(ai, goal, 3010, 10, local0[7], 0)
-    local0[7] = SetCoolTime(ai, goal, 3011, 10, local0[7], 0)
-    local0[11] = SetCoolTime(ai, goal, 6002, 5, local0[11], 0)
-    local0[11] = SetCoolTime(ai, goal, 6003, 5, local0[11], 0)
-    local1[1] = REGIST_FUNC(ai, goal, MoujaDog202000_Act01)
-    local1[2] = REGIST_FUNC(ai, goal, MoujaDog202000_Act02)
-    local1[3] = REGIST_FUNC(ai, goal, MoujaDog202000_Act03)
-    local1[4] = REGIST_FUNC(ai, goal, MoujaDog202000_Act04)
-    local1[5] = REGIST_FUNC(ai, goal, MoujaDog202000_Act05)
-    local1[6] = REGIST_FUNC(ai, goal, MoujaDog202000_Act06)
-    local1[7] = REGIST_FUNC(ai, goal, MoujaDog202000_Act07)
-    local1[10] = REGIST_FUNC(ai, goal, MoujaDog202000_Act10)
-    local1[11] = REGIST_FUNC(ai, goal, MoujaDog202000_Act11)
-    local1[20] = REGIST_FUNC(ai, goal, MoujaDog202000_Act20)
-    Common_Battle_Activate(ai, goal, local0, local1, REGIST_FUNC(ai, goal, MoujaDog202000_ActAfter_AdjustSpace), local2)
+    
+    actChanceList[3] = SetCoolTime(ai, goal, 3005, 10, actChanceList[3], 0)
+    actChanceList[7] = SetCoolTime(ai, goal, 3010, 10, actChanceList[7], 0)
+    actChanceList[7] = SetCoolTime(ai, goal, 3011, 10, actChanceList[7], 0)
+    actChanceList[11] = SetCoolTime(ai, goal, 6002, 5, actChanceList[11], 0)
+    actChanceList[11] = SetCoolTime(ai, goal, 6003, 5, actChanceList[11], 0)
+    
+    actFuncList[1] = REGIST_FUNC(ai, goal, SpellSummon_UndeadDog_Act01)
+    actFuncList[2] = REGIST_FUNC(ai, goal, SpellSummon_UndeadDog_Act02)
+    actFuncList[3] = REGIST_FUNC(ai, goal, SpellSummon_UndeadDog_Act03)
+    actFuncList[4] = REGIST_FUNC(ai, goal, SpellSummon_UndeadDog_Act04)
+    actFuncList[5] = REGIST_FUNC(ai, goal, SpellSummon_UndeadDog_Act05)
+    actFuncList[6] = REGIST_FUNC(ai, goal, SpellSummon_UndeadDog_Act06)
+    actFuncList[7] = REGIST_FUNC(ai, goal, SpellSummon_UndeadDog_Act07)
+    actFuncList[10] = REGIST_FUNC(ai, goal, SpellSummon_UndeadDog_Act10)
+    actFuncList[11] = REGIST_FUNC(ai, goal, SpellSummon_UndeadDog_Act11)
+    actFuncList[20] = REGIST_FUNC(ai, goal, SpellSummon_UndeadDog_Act20)
+    
+    Common_Battle_Activate(ai, goal, actChanceList, actFuncList, REGIST_FUNC(ai, goal, SpellSummon_UndeadDog_ActAfter_AdjustSpace), actTblList)
     return 
 end
 
-function MoujaDog202000_Act01(self, ai, goal)
-    local local0 = 2.4 - self:GetMapHitRadius(TARGET_SELF)
-    local local1 = 2.4 - self:GetMapHitRadius(TARGET_SELF) + 1
-    local local2 = 3000
-    if local1 < self:GetDist(TARGET_ENE_0) then
-        local0 = local0 + 1
-        local2 = 3007
+-- Bite
+function SpellSummon_UndeadDog_Act01(self, ai, goal)
+    local start_approach_distance_close = 2.4
+    local start_approach_distance_mid = start_approach_distance_close + 0
+    local start_approach_distance_far = start_approach_distance_close + 1
+    
+    local roll_for_mid_distance_to_use_duration_far = 0
+    local roll_for_L1_use = 0
+    
+    local approach_duration_close = 1.5
+    local approach_duration_far = 4.0
+    
+    local anim_ForwardBite = 3000
+    local anim_ForwardBite2 = 3007
+    local anim_LeftBite = 3001
+    local anim_Current = anim_ForwardBite
+    
+    if self:GetDist(TARGET_ENE_0) >= 2.5 then
+        anim_Current = 3007
     end
-    Approach_Act_Flex(self, ai, local0, 2.4 - self:GetMapHitRadius(TARGET_SELF) + 0, local1, 0, 0, 1.5, 4)
-    ai:AddSubGoal(GOAL_COMMON_ComboAttackTunableSpin, 10, local2, TARGET_ENE_0, 2.2 - self:GetMapHitRadius(TARGET_SELF) + 1, 0, 0, 0, 0)
+    
+    -- Approach if enemy exceeds approach distance
+    NPC_Approach_Act_Flex(self, ai, start_approach_distance_close, start_approach_distance_mid, start_approach_distance_far, roll_for_mid_distance_to_use_duration_far, roll_for_L1_use, approach_duration_close, approach_duration_far)
+    
+    ai:AddSubGoal(GOAL_COMMON_ComboAttackTunableSpin, 10, anim_Current, TARGET_ENE_0, 2.2 - self:GetMapHitRadius(TARGET_SELF) + 1, 0, 0, 0, 0)
+    
     if self:GetRandam_Int(1, 100) < 80 then
-        ai:AddSubGoal(GOAL_COMMON_ComboFinal, 10, 3001, TARGET_ENE_0, 3.5 - self:GetMapHitRadius(TARGET_SELF), 0, 0)
+        ai:AddSubGoal(GOAL_COMMON_ComboFinal, 10, anim_LeftBite, TARGET_ENE_0, 3.5 - self:GetMapHitRadius(TARGET_SELF), 0, 0)
     end
+    
     GetWellSpace_Odds = 100
     return GetWellSpace_Odds
 end
 
-function MoujaDog202000_Act02(self, ai, goal)
+-- Jump Bite
+function SpellSummon_UndeadDog_Act02(self, ai, goal)
     local local0 = 3.2 - self:GetMapHitRadius(TARGET_SELF)
     local local1 = 3.2 - self:GetMapHitRadius(TARGET_SELF) + 1
     local local2 = 3002
@@ -123,13 +152,13 @@ function MoujaDog202000_Act02(self, ai, goal)
     return GetWellSpace_Odds
 end
 
-function MoujaDog202000_Act03(self, ai, goal)
+function SpellSummon_UndeadDog_Act03(self, ai, goal)
     ai:AddSubGoal(GOAL_COMMON_AttackTunableSpin, 10, 3005, TARGET_ENE_0, 3.5 - self:GetMapHitRadius(TARGET_SELF), 0, 0, 0, 0)
     GetWellSpace_Odds = 0
     return GetWellSpace_Odds
 end
 
-function MoujaDog202000_Act04(self, ai, goal)
+function SpellSummon_UndeadDog_Act04(self, ai, goal)
     local local0 = 2.5 - self:GetMapHitRadius(TARGET_SELF)
     local local1 = 2.5 - self:GetMapHitRadius(TARGET_SELF) + 1
     local local2 = 3008
@@ -143,7 +172,7 @@ function MoujaDog202000_Act04(self, ai, goal)
     return GetWellSpace_Odds
 end
 
-function MoujaDog202000_Act05(self, ai, goal)
+function SpellSummon_UndeadDog_Act05(self, ai, goal)
     local local0 = 7 - self:GetMapHitRadius(TARGET_SELF)
     local local1 = 7 - self:GetMapHitRadius(TARGET_SELF) + 1
     if local1 < self:GetDist(TARGET_ENE_0) then
@@ -155,7 +184,7 @@ function MoujaDog202000_Act05(self, ai, goal)
     return GetWellSpace_Odds
 end
 
-function MoujaDog202000_Act06(self, ai, goal)
+function SpellSummon_UndeadDog_Act06(self, ai, goal)
     local local0 = 6.6 - self:GetMapHitRadius(TARGET_SELF)
     local local1 = 6.6 - self:GetMapHitRadius(TARGET_SELF) + 1
     if local1 < self:GetDist(TARGET_ENE_0) then
@@ -167,7 +196,7 @@ function MoujaDog202000_Act06(self, ai, goal)
     return GetWellSpace_Odds
 end
 
-function MoujaDog202000_Act07(self, ai, goal)
+function SpellSummon_UndeadDog_Act07(self, ai, goal)
     local local0 = 3.5 - self:GetMapHitRadius(TARGET_SELF)
     local local1 = 0
     local local2 = 0
@@ -180,13 +209,13 @@ function MoujaDog202000_Act07(self, ai, goal)
     return GetWellSpace_Odds
 end
 
-function MoujaDog202000_Act10(self, ai, goal)
+function SpellSummon_UndeadDog_Act10(self, ai, goal)
     ai:AddSubGoal(GOAL_COMMON_MoveToSomewhere, self:GetRandam_Float(1.5, 2), TARGET_ENE_0, self:GetRandam_Int(AI_DIR_TYPE_ToL, AI_DIR_TYPE_ToR), 6, TARGET_SELF, false)
     GetWellSpace_Odds = 0
     return GetWellSpace_Odds
 end
 
-function MoujaDog202000_Act11(self, ai, goal)
+function SpellSummon_UndeadDog_Act11(self, ai, goal)
     if InsideRange(self, ai, 90, 180, -9999, 9999) then
         ai:AddSubGoal(GOAL_COMMON_SpinStep, 5, 6002, TARGET_ENE_0, 0, AI_DIR_TYPE_L, 5)
     elseif InsideRange(self, ai, -90, 180, -9999, 9999) then
@@ -196,13 +225,13 @@ function MoujaDog202000_Act11(self, ai, goal)
     return GetWellSpace_Odds
 end
 
-function MoujaDog202000_Act20(self, ai, goal)
+function SpellSummon_UndeadDog_Act20(self, ai, goal)
     ai:AddSubGoal(GOAL_COMMON_Turn, 2, TARGET_ENE_0, 90)
     GetWellSpace_Odds = 0
     return GetWellSpace_Odds
 end
 
-function MoujaDog202000_ActAfter_AdjustSpace(self, ai, goal)
+function SpellSummon_UndeadDog_ActAfter_AdjustSpace(self, ai, goal)
     ai:AddSubGoal(GOAL_SpellSummon_UndeadDog_801200_AfterAttackAct, 10)
     return 
 end
@@ -220,7 +249,7 @@ Goal.Interrupt = function (self, ai, goal)
         return false
     elseif ai:GetSpecialEffectActivateInterruptType(0) == 12012 then
         goal:ClearSubGoal()
-        MoujaDog202000_Act03(ai, goal, paramTbl)
+        SpellSummon_UndeadDog_Act03(ai, goal, paramTbl)
         return true
     else
         return 
